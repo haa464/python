4e7c7418f3d1c1c8f20c0c593a8ab394ca4c5d7b
haa464 <you@example.com>
2020-07-26 19:36:48 +0530

    first game program

--- Automate Python project/Automate Spotify With Python/run.py
+++ Automate Python project/Automate Spotify With Python/run.py
@@ -1,5 +1,6 @@
 import os
 
+
 from spotify_client import SpotifyClient
 from youtube_client import YouTubeClient
 
--- Automate Python project/Automate Spotify With Python/spotify_client.py
+++ Automate Python project/Automate Spotify With Python/spotify_client.py
@@ -1,43 +1,42 @@
 import requests
 import urllib.parse
 
+
 class SpotifyClient(object):
-	def __init__(self, api_taken):
-		self.api_taken = api_taken
+    def __init__(self, api_token):
+        self.api_token = api_token
+    
+    def search_song(self, artist, track):
+        query = urllib.parse.quote(f'{artist} {track}')
+        url = f"https://api.spotify.com/v1/search?q={query}&type=track"
+        response = requests.get(
+            url,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {self.api_token}"
+            }
+        )
+        response_json = response.json()
+        
+        results = response_json['tracks']['items']
+        if results:
+            # let's assume the first track in the list is the song we want
+            return results[0]['id']
+        else:
+            raise Exception(f"No song found for {artist} = {track}")
+    
+    def add_song_to_spotify(self, song_id):
+        url = "https://api.spotify.com/v1/me/tracks"
+        response = requests.put(
+            url,
+            json={
+                "ids": [song_id]
+            },
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {self.api_token}"
+            }
+        )
+        
+        return response.ok
 		
-
-	def search_song(self, artist, track):
-		query = urllib.prase.quote(f'{artist} {track}')
-		url = f"http://api.spofity.com/v1/search?q={query}&type=track"
-		response = requests.get(
-			url,
-			header={
-				"content-type": "application/json",
-				"Authorization": f"Bearer {self.api_taken}"
-			}
-		)
-		response_json = response.json()
-
-		result = response_json['track']['items']
-		if results:
-
-			 return results[0]['id']
-		else:
-			raise Exception(f"No song found for {artist} = {track}")
-
-	def add_song_to_spofity(self, song_id):
-		url = "https://api.spofity.com/v1/me/tracks"
-
-		response = response.put(
-
-			url,
-			json={
-				"ids": [song_id]
-			},
-			header={
-				"Content-Type": "application/json",
-				"Authorization": f"Bearer {self.api_taken}"
-			}
-		)
-
-		return response.ok
